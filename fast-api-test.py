58a242890dc1d072c95045189a108e473eefe483
schleising <stephen@schleising.net>
2022-12-03 15:16:06 +0000

    Add examples

--- fast-api-test.py
+++ fast-api-test.py
@@ -5,11 +5,33 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class Thing(BaseModel):
+    age: int
+    height: float
+
+    class Config:
+        schema_extra = {
+            'example': {
+                'age': 42,
+                'height': 165.78,
+            }
+        }
 
 class Item(BaseModel):
     name: str
     price: float
+    thing: Thing
     is_offer: Union[bool, None] = None
+
+    class Config:
+        schema_extra = {
+            'example': {
+                'name': 'John',
+                'price': 77.54,
+                'thing': Thing.Config.schema_extra['example'],
+                'is_offer': False,
+            }
+        }
 
 
 @app.get("/")
